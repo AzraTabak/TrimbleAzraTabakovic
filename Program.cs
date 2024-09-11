f65bce2503081431f9a566e3b462b6ebbf805a22
AzraTabak <tabakovic.azra99@gmail.com>
2024-09-11 23:34:53 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 
 public class Point2D
@@ -49,8 +50,8 @@ public class Circular
     public Point2D CalculateCenter()
     {
 
-        double centerX = StartPoint.X + Radius * Math.Sin(Math.PI/2-StartDirection);
-        double centerY = StartPoint.Y - Radius * Math.Cos(Math.PI/2 - StartDirection);
+        double centerX = StartPoint.X + Radius * Math.Sin(Math.PI / 2 - StartDirection);
+        double centerY = StartPoint.Y - Radius * Math.Cos(Math.PI / 2 - StartDirection);
         return new Point2D(centerX, centerY);
     }
     public Point2D EvaluateAt(double length)
@@ -62,25 +63,24 @@ public class Circular
         double newAngle = length / Math.Abs(Radius);
         if (Radius >= 0)
         {
-            newX = center.X + Radius * Math.Sin(newAngle +StartDirection-Math.PI/2);
-            newY = center.Y + Radius* Math.Cos(newAngle +StartDirection-Math.PI/2);
+            newX = center.X + Radius * Math.Sin(newAngle + StartDirection - Math.PI / 2);
+            newY = center.Y + Radius * Math.Cos(newAngle + StartDirection - Math.PI / 2);
         }
         else
         {
             newX = center.X - Math.Abs(Radius) * Math.Sin(newAngle - StartDirection - Math.PI / 2);
-            newY = center.Y + Math.Abs(Radius) * Math.Cos(newAngle -StartDirection- Math.PI / 2);
+            newY = center.Y + Math.Abs(Radius) * Math.Cos(newAngle - StartDirection - Math.PI / 2);
 
         }
         return new Point2D(newX, newY);
     }
 }
-
 public class Clothoid
 {
-    Point2D StartPoint;
-    public double StartDirection; 
-    public double StartCurvature; 
-    public double EndCurvature;   
+    public Point2D StartPoint;
+    public double StartDirection;
+    public double StartCurvature;
+    public double EndCurvature;
     public double Length;
 
     public Clothoid(Point2D startPoint, double startDirection, double startCurvature, double endCurvature, double length)
@@ -92,9 +92,37 @@ public class Clothoid
         Length = length;
     }
 
+    public Point2D EvaluateAt(double length)
+    {
+       
+        
+        double curvatureStep = (EndCurvature - StartCurvature) / Length;
+
+        double currentX = StartPoint.X;
+        double currentY = StartPoint.Y;
+        double currentDirection = StartDirection;
+
+        
+        double stepSize = 0.1; 
+        for (double s = 0; s < length; s += stepSize)
+        {
+            
+            double currentCurvature = StartCurvature + curvatureStep * s;
+
+            currentDirection += currentCurvature * stepSize;
+
+            currentX += stepSize * Math.Cos(currentDirection);
+            currentY += stepSize * Math.Sin(currentDirection);
+        }
+
+        return new Point2D(currentX, currentY);
+    }
 }
 
-    public class Program
+
+
+
+public class Program
 {
     public static void Main()
     {
@@ -104,11 +132,16 @@ public class Clothoid
         //here we can add an  opportunity to input different values and in that case we would take values only if they are >=0 (length)
         Console.WriteLine("New point on straight is ( " + newPointOnStraight.X + "   ,   " + newPointOnStraight.Y + ")");
 
-
         var circularSegment = new Circular(new Point2D(4364344.213545, 5526822.467083), 0.7156848870803333, 351.6);
         var pointOnArc = circularSegment.EvaluateAt(409.716808);
 
-        Console.WriteLine($"New point on circural arc is ( {pointOnArc.X}   ,   {pointOnArc.Y})");
+        Console.WriteLine($"New point on circural arc is ( {pointOnArc.X}   ,   {pointOnArc.Y}) ");
+        
+        var clothoidSegment = new Clothoid(newPointOnStraight, 0.6488475748464434, 0, 0.002844141069397042, 470);
+        double clothoidLength = 300.0;
+        var pointOnClothoid = clothoidSegment.EvaluateAt(clothoidLength);
+
+        Console.WriteLine($"New point on clothoid is ( {pointOnClothoid.X}   ,   {pointOnClothoid.Y}) ");
         Console.ReadKey();
 
     }
